879257847bbfd961fb9b11ae526a939f4eefc6e1
Dhruv Bhargav <dhruvbhargav@Dhruvs-MacBook-Pro.local>
2024-06-18 18:13:07 +0530

    added student teacher relation

--- home/models.py
+++ home/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    email = models.CharField(max_length = 50,default='',primary_key=True,null=False)
-    name = models.CharField(max_length = 50,default='',null=False)
+class Student(models.Model):
+    email = models.CharField(max_length = 32,default='',primary_key=True,null=False)
+    name = models.CharField(max_length = 16,default='',null=False)
     password = models.CharField(max_length = 256,default='',null=False)
+    standard = models.IntegerField(max_length=8,default=0,null=False)
+
+class Teacher(models.Model):
+    email = models.CharField(max_length = 32,default='',primary_key=True,null=False)
+    name = models.CharField(max_length = 16,default='',null=False)
+    password = models.CharField(max_length = 256,default='',null=False)
+    subject = models.CharField(max_length=16,default='',null=False)
+
+class Student_Teacher_Relation(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+
+
--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
-from home.views import Add_User,Get_All_User,Get_User,Update_User,Delete_User
+from home.views import Add_User,Get_All_Student,Get_All_Teacher,Update_Student,Update_Teacher,Delete_Student,Delete_Teacher,Assign_Student_Teacher
 
 urlpatterns = [
     path("add_user",Add_User.as_view()),
-    path("get_all_user",Get_All_User.as_view()),
-    path("get_user",Get_User.as_view()),
-    path("update_user",Update_User.as_view()),
-    path("delete_user",Delete_User.as_view())
+    path("get_all_student",Get_All_Student.as_view()),
+    path("get_all_teacher",Get_All_Teacher.as_view()),
+    path("update_student",Update_Student.as_view()),
+    path("update_teacher",Update_Teacher.as_view()),
+    path("delete_student",Delete_Student.as_view()),
+    path("delete_teacher",Delete_Teacher.as_view()),
+    path("assign_student_teacher",Assign_Student_Teacher.as_view()),
 ]
